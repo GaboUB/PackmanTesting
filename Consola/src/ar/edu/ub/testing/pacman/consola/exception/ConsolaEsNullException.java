1477d5f470895d8d276e2f580c700f32f4664da9
Gabriel <38113740+GabrielEspina@users.noreply.github.com>
2018-04-20 22:13:54 -0300

    Consola de Pacman
    Subo mi parte de Consola para el juego Pacman.

---
+++ Consola/src/ar/edu/ub/testing/pacman/consola/exception/ConsolaEsNullException.java
@@ -0,0 +1,12 @@
+package ar.edu.ub.testing.pacman.consola.exception;
+
+public class ConsolaEsNullException extends ConsolaException
+{
+	private static final long serialVersionUID = 1L;
+	
+	public ConsolaEsNullException(String msg)
+	{
+		super(msg);
+	}
+
+}
